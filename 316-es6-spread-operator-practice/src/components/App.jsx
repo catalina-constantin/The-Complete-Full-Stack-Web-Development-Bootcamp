544d371426fab876661960c348c77b80d6e2ad58
Constantin Catalina <catalina.constantin@cognizant.com>
2026-02-20 22:12:10 +0200

    add solution for 304, 307, 316 and 318 ex

---
+++ 316-es6-spread-operator-practice/src/components/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
+
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            setItems([...items, input]);
+            setInput("");
+          }}
+        >
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((item, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={item}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
